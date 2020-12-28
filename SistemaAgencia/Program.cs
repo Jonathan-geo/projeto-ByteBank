05456b4bce73796bff5cf80d93666ecb0fe48fac
Jonathan Cardoso Lopes Domingos <jonathan.domingos@sondait.com.br>
2020-12-28 15:45:33 -0300

    concentrando arquivos na biblioteca

---
+++ SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(conta.Agencia);
+            Console.ReadLine();
+        }
+    }
+}
